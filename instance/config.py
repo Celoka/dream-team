515b461c9ae05640e063bd54804f82cb399d0578
Celoka <eloka.chima@gmail.com>
2018-01-25 22:48:27 +0100

    feat(auth):setup base for project
    - setup database
    - create respective models
    - run migrations
    - create blueprints for flask components

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,2 @@
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI ='mysql://dt_admin:dt2018@localhost/dreamteam_db'
+SECRET_KEY = 'SecretKey'
+SQLALCHEMY_DATABASE_URI ='postgres://postgres:Asorock1@localhost/dreamteam_db'
