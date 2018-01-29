fe4c78275b8f445e9d98fdba1bd6162c12b69b60
Celoka <eloka.chima@gmail.com>
2018-01-29 19:35:53 +0100

    feat(): implement error pages
    - create error pages by creating an error directory in the template
    - edit app initialization file

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask import abort, Flask, render_template
 
 # local imports
 from config import app_config
@@ -35,5 +36,17 @@ def create_app(config_name):
 
     from .home import home as home_blueprint 
     app.register_blueprint(home_blueprint)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'),403
+    
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'),404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'),500
 
     return app
