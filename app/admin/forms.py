ba5f808059fe5e1991df9bf26c834a2ec12d1d35
Celoka <eloka.chima@gmail.com>
2018-01-29 12:05:45 +0100

    feat(): implement listing of employees
    - create and edit form to assign each employee a department and role
    - create employee directory in template

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired 
+from wtforms.ext.sqlalchemy.fields import QuerySelectField 
+
+from ..models import Department, Role 
+
 
 class DepartmentForm(FlaskForm):
     """
@@ -17,4 +21,14 @@ class RoleForm(FlaskForm):
 
     name = StringField('Name', validators=[DataRequired()])
     description =StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class EmployeeAssignForm(FlaskForm):
+    """
+    Form for admin to assing departments and roles to employees
+    """
+    department = QuerySelectField(query_factory=lambda: Department.query.all(),
+                                  get_label="name", get_pk=lambda x:"id")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(),
+                            get_label="name", get_pk=lambda x:"id")
     submit = SubmitField('Submit')
